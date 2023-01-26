9461b9858f807caeffe1d52093dd41214c837d5f
Mithun <mithuncm@gmail.com>
2023-01-26 23:07:28 +0530

    A bug due to an edge case is fixed. Also added code to get the time elapsed to execute the sort

--- src/main/java/uber/kautilya/challenges/quicksort/QuickSortCustomPivotZero.java
+++ src/main/java/uber/kautilya/challenges/quicksort/QuickSortCustomPivotZero.java
@@ -1,12 +1,17 @@
 package uber.kautilya.challenges.quicksort;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class QuickSortCustomPivotZero {
     public static void main(String[] args) {
 
-        int[] array = new int[]{2, 3, 5, 9, 1, 7, 8, 4};
+        int[] array = IntStream.range(1, 1000).boxed().sorted((a, b) -> -1).mapToInt(Integer::intValue).toArray();
+        Instant now = Instant.now();
         quickSort(array, 0, (array.length - 1));
+        System.out.printf("\nTime taken: %s", Duration.between(now, Instant.now()));
         System.out.print("\nFinal sorted array: " + Arrays.toString(array));
 
         System.out.println();
@@ -20,7 +25,7 @@ public class QuickSortCustomPivotZero {
             // Partitioning index
             int pivotIndex = getPivotIndexAndPartition(array, lowerIndex, highIndex);
             System.out.printf("\n%d is correctly assigned in array: %s", array[pivotIndex],
-                    Arrays.toString(Arrays.copyOfRange(array, lowerIndex, highIndex)));
+                    Arrays.toString(Arrays.copyOfRange(array, lowerIndex, highIndex + 1)));
             quickSort(array, lowerIndex, pivotIndex - 1);
             quickSort(array, pivotIndex + 1, highIndex);
         }
@@ -33,10 +38,11 @@ public class QuickSortCustomPivotZero {
      * Returns the position at which the correct value has been placed
      */
     static int getPivotIndexAndPartition(int[] arr, int minIndex, int maxIndex) {
+//        System.out.printf("\narr: %s, minIndex: %d, maxIndex: %d", Arrays.toString(Arrays.copyOfRange(arr, minIndex, maxIndex + 1)), minIndex, maxIndex);
         int pivotValue = arr[minIndex];
         int leftIndex = minIndex + 1;
         int rightIndex = maxIndex;
-        while (leftIndex < rightIndex) {
+        while (leftIndex <= rightIndex) {
             while (leftIndex <= rightIndex && arr[leftIndex] <= pivotValue) {
                 leftIndex++;
             }
@@ -60,7 +66,7 @@ public class QuickSortCustomPivotZero {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-        System.out.printf("\nSwapped %d with %d. %s%s", array[j], array[i], "New array after swap: ", Arrays.toString(array));
+//        System.out.printf("\nSwapped %d with %d. %s%s", array[j], array[i], "New array after swap: ", Arrays.toString(array));
         return j;
     }
 }
